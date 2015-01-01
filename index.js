d9993346c32c75fb448ab5ee59fb215c60679542
Henri Jouhaud <henri.jouhaud@polyconseil.fr>
2015-01-01 18:01:31 +0100

    Correct root index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var careless = require('./careless');
-var tools = require('./tools');
-
-module.exports = {
-  createElement: careless.createElement,
-  createClass: careless.createClass,
-  renderToString: careless.renderToString,
-  Component: careless.Component,
-  raw: careless.raw,
-  escapeXml: careless.escapeXml,
-  __spread: careless.__spread,
-  tools: tools
-};
+module.exports = require('./dist');
